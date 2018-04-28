12c315a09175fe2fa29a2de73f7c251f43b05710
furixturi <alabebop@gmail.com>
2018-04-28 21:10:23 +0900

    for loop a slice

--- cards/main.go
+++ cards/main.go
@@ -8,7 +8,9 @@ func main() {
 	cards := []string{"Ace of Diamonds", newCard()} // A Slice of type string containing elements in {}
 	cards = append(cards, "Six of Spades")          // append function doesn't modify the original slice but returns a new slice
 
-	fmt.Println(cards)
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
