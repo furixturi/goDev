55c4cb0a8275e738b1742a54712cdc3036f6baaf
furixturi <alabebop@gmail.com>
2018-04-28 21:35:12 +0900

    add a custom type and use it in main.go

---
+++ cards/deck.go
@@ -0,0 +1,5 @@
+package main
+
+// Create a new type of "deck"
+// which is essentially a slice of strings
+type deck []string
--- cards/main.go
+++ cards/main.go
@@ -5,8 +5,11 @@ import (
 )
 
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()} // A Slice of type string containing elements in {}
-	cards = append(cards, "Six of Spades")          // append function doesn't modify the original slice but returns a new slice
+	// to use the new type deck
+	// now we need to say
+	// go run main.go deck.go
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
 
 	for i, card := range cards {
 		fmt.Println(i, card)
