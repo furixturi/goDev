bb5dd3bbaee0e3fb95ebc415024f11a5af1436bb
furixturi <alabebop@gmail.com>
2018-04-29 08:47:19 +0900

    type conversion

--- cards/deck.go
+++ cards/deck.go
@@ -4,19 +4,14 @@ import (
 	"fmt"
 )
 
-// Create a new type of "deck"
-// which is essentially a slice of strings
 type deck []string
 
-// function to create a new deck
-// returns a deck
 func newDeck() deck {
 	cards := deck{} // an empty deck slice
 
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
-	// "_" tells go compiler that we know here is a variable but we don't need to use it
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
 			cards = append(cards, value+" of "+suit)
@@ -32,9 +27,10 @@ func (d deck) print() {
 	}
 }
 
-// return multiple values from one function
 func deal(d deck, handSize int) (deck, deck) {
-	// aSlice[startIndex:endButNotIncludedIndex]
-	// creates a new reference which points to a subsection of a slice
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	[]string(d) // convert deck d to a slice of type string * type conversion: type(var)
 }
