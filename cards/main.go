64f6d11d5a799ee5c464ca1722b812ca5a40aecb
furixturi <alabebop@gmail.com>
2018-04-28 14:59:36 +0900

    add a function with return type and use it in main

--- cards/main.go
+++ cards/main.go
@@ -5,17 +5,11 @@ import (
 )
 
 func main() {
-	// initialize a variable and explicitly declare its type,
-	// then assign a value to it
-	// usable within and outside of functions
-	// var card = "Ace of Spades"
-
-	// := initializes and assigns the value of a new variable
-	// letting the compiler infer its type by the assigned value
-	// be careful not to use := twice on the same variable name
-	// otherwise there will be a compiler error
-	// Warning: only usable within a function
-	card := "Ace of Spades"
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Ace of Spades"
 }
