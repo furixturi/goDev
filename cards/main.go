93a90e328161dc8d24a2dd725b540d7439bb53d1
furixturi <alabebop@gmail.com>
2018-04-28 21:08:41 +0900

    Slice: flexible length array in golang

--- cards/main.go
+++ cards/main.go
@@ -5,9 +5,10 @@ import (
 )
 
 func main() {
-	card := newCard()
+	cards := []string{"Ace of Diamonds", newCard()} // A Slice of type string containing elements in {}
+	cards = append(cards, "Six of Spades")          // append function doesn't modify the original slice but returns a new slice
 
-	fmt.Println(card)
+	fmt.Println(cards)
 }
 
 func newCard() string {
