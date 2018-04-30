7c1d66995b8d7ba5a12a36b07aa1e3a490b83dbd
furixturi <alabebop@gmail.com>
2018-04-30 22:25:28 +0900

    Read a txt file that the user gave and print its content in console

--- assignment3_readFromFile/main.go
+++ assignment3_readFromFile/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
-	//[/var/folders/6p/_z0jx7v974v_wsql5n2t79jw0000gn/T/go-build553020332/b001/exe/main wow wow wow]
-	// first is the temporary address of the compiled main.go
-	// after that are the words the user typed
-	fmt.Println(os.Args)
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	io.Copy(os.Stdout, f)
 }
