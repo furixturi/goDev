5f6ef2c72a881f13391fdf68707515ea9579d0d4
furixturi <alabebop@gmail.com>
2018-04-30 21:13:13 +0900

    Naive implementation of our own Writer type which logs something more than io.Stdout does

--- http/main.go
+++ http/main.go
@@ -7,12 +7,23 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	res, err := http.Get("http://google.com")
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
-	// os.Stdout implement the Writer interface, res.Body implements the Reader interface
-	io.Copy(os.Stdout, res.Body)
+
+	lw := logWriter{}
+	io.Copy(lw, res.Body)
+
+}
+
+// by implementing this function, logWriter implements the Writer interface
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes: ", len(bs))
+	return len(bs), nil
 }
